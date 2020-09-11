d959076cfab0ead80a85d6c9f5e03e3fd80c5e81
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-09-11 05:50:14 +0000

    Iterating over a packing list array with a for loop

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packinglist = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
